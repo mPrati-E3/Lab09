c1190dd8b5182e9bec6d5049d14ac7592b166256
MPrati <MPrati@HP-250-G8-MP.homenet.telecomitalia.it>
2021-04-02 12:19:42 +0200

    Interfaccia grafica + mvc

--- src/main/java/it/polito/tdp/borders/FXMLController.java
+++ src/main/java/it/polito/tdp/borders/FXMLController.java
@@ -1,4 +1,3 @@
-
 package it.polito.tdp.borders;
 
 import java.net.URL;
@@ -7,38 +6,57 @@ import java.util.ResourceBundle;
 import it.polito.tdp.borders.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class FXMLController {
-
-	private Model model;
 	
-    @FXML // ResourceBundle that was given to the FXMLLoader
+	private Model model;
+
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // fx:id="txtAnno"
-    private TextField txtAnno; // Value injected by FXMLLoader
+    @FXML
+    private HBox dropAnno;
 
-    @FXML // fx:id="txtResult"
-    private TextArea txtResult; // Value injected by FXMLLoader
+    @FXML
+    private Button btnCalcolaConfini;
+
+    @FXML
+    private ChoiceBox<?> dropStato;
+
+    @FXML
+    private Button btnTrovaTuttiVicini;
+
+    @FXML
+    private TextArea txtResult;
 
     @FXML
     void doCalcolaConfini(ActionEvent event) {
 
     }
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    void doTrovaTuttiVicini(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
-        assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert dropAnno != null : "fx:id=\"dropAnno\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCalcolaConfini != null : "fx:id=\"btnCalcolaConfini\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert dropStato != null : "fx:id=\"dropStato\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnTrovaTuttiVicini != null : "fx:id=\"btnTrovaTuttiVicini\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
     
-    public void setModel(Model model) {
-    	this.model = model;
+    public void setModel (Model m) {
+    	this.model=m;
     }
 }
